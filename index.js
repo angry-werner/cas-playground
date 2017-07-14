330ad0f9034b0107f909b6f772b66fcedff6edc2
kow <werner.koch@bluewin.ch>
2017-07-14 23:03:29 +0200

    (kow) Nicht gerade viel.

--- index.js
+++ index.js
@@ -10,35 +10,34 @@ const tone_analyzer = new ToneAnalyzerV3(credentials.toneAnalyser);
 const farts = new PouchDB('farts');
 
 const stream = client.stream('statuses/filter', {follow: '254595701'});
-stream.on('data', function (event) {
+stream.on('data', function (tweet) {
     'use strict';
-
-    function analyse(tweet) {
-        'use strict';
-
-        tone_analyzer.tone(tweet, function (err, tone) {
-            if (err) {
-                console.log(err);
-            } else {
-                var toneFromWatson = tone;
-                var id = uuidv1();
-                var item = {
-                    '_id': id,
-                    'message': event,
-                    'sentiment': toneFromWatson
-                };
-
-                farts.put(item);
-                farts.get(id).then(function (document) {
-                    console.log(JSON.stringify(document, null, 2));
-                });
-            }
-        });
-    }
-    analyse({'text': event.text});
+    analyse(tweet);
 });
 
 stream.on('error', function (error) {
     throw error;
 });
 
+function analyse(tweet) {
+    'use strict';
+
+    tone_analyzer.tone({'text': tweet.text}, function (err, tone) {
+        if (err) {
+            console.log(err);
+        } else {
+            var toneFromWatson = tone;
+            var id = uuidv1();
+            var item = {
+                '_id': id,
+                'message': tweet,
+                'sentiment': toneFromWatson
+            };
+
+            farts.put(item);
+            farts.get(id).then(function (document) {
+                console.log(JSON.stringify(document, null, 2));
+            });
+        }
+    });
+}
